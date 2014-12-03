6cce6191102402749889f8a42dc1aad674d8b3be
san <xyesan@gmail.com>
2014-12-03 21:19:44 +0800

    check lua configure file existence

--- parseconf.cpp
+++ parseconf.cpp
@@ -1,26 +1,47 @@
 #include "parseconf.h"
 #include <QDir>
+#include <QFile>
+#include <QApplication>
 
-void ShowError(QString title, QString text)
+void ErrorExit(QString title, QString text)
 {
     QMessageBox::critical(NULL, title, text);
+    qApp->exit();
 }
+
+QString GetFullCfgPath()
+{
+    QDir dir;
+    return dir.absolutePath() + QDir::separator() + QString::fromUtf8(CONFIG_FILE);
+}
+
+bool ConfExist()
+{
+    QFile f;
+
+    f.setFileName(GetFullCfgPath());
+
+    return f.exists();
+}
+
 
 void ParseConf(QString *host, int *port, QString *backend)
 {
     bool isErr;
     const char *h, *b;
-    QString errMsg, cur;
-    QDir dir;
+    QString errMsg;
 
-    cur = dir.absolutePath();
+    if(false == ConfExist()) {
+        errMsg = "Can not open configure file '" + GetFullCfgPath() + "'";
+        ErrorExit("Configure", errMsg);
+    }
 
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
     isErr = luaL_dofile(L, CONFIG_FILE);
     if(isErr == true) {
-        errMsg = "Open configure file '" + cur + QDir::separator() + QString::fromUtf8(CONFIG_FILE) + "' failed";
-        ShowError("Configure", errMsg);
+        errMsg = "Can not open configure file '" + GetFullCfgPath() + "'";
+        ErrorExit("Configure", errMsg);
     }
 
     lua_getglobal(L, "HOST");
@@ -29,17 +50,17 @@ void ParseConf(QString *host, int *port, QString *backend)
 
     if (!lua_isstring(L, -3)) {
         lua_close(L);
-        ShowError("Configure", "'HOST' should be a string");
+        ErrorExit("Configure", "'HOST' should be a string");
     }
 
     if (!lua_isnumber(L, -2)) {
         lua_close(L);
-        ShowError("Configure", "'PORT' should be a number");
+        ErrorExit("Configure", "'PORT' should be a number");
     }
 
     if (!lua_isstring(L, -1)) {
         lua_close(L);
-        ShowError("Configure", "'BACKEND' should be a string");
+        ErrorExit("Configure", "'BACKEND' should be a string");
     }
 
     h = lua_tostring(L, -3);
@@ -55,14 +76,22 @@ void ParseConf(QString *host, int *port, QString *backend)
 int MaxPktSize(void)
 {
     int size;
+    QString errMsg;
+
+    if(false == ConfExist()) {
+        errMsg = "Can not open configure file '" + GetFullCfgPath() + "'";
+        ErrorExit("Configure", errMsg);
+    }
 
     lua_State *L = luaL_newstate();
     luaL_dofile(L, CONFIG_FILE);
 
     lua_getglobal(L, "MAX_PKG_SIZE");
 
-    if (!lua_isnumber(L, -1))
-        luaL_error(L, "'PORT' should be a number\n");
+    if (!lua_isnumber(L, -1)) {
+        lua_close(L);
+        ErrorExit("Configure", "'MAX_PKG_SIZE' should be a string");
+    }
 
     size = lua_tointeger(L, -1);
 
